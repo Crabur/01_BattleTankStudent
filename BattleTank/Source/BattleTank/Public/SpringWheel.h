88192d4e882fb4f19eed652b65c7d5fc2f82ff36
CRABOTAUR\Crabur <nuvamajor@gmailcom>
2018-07-13 14:35:38 -0700

    Started adding suspension, pre-axle.

---
+++ BattleTank/Source/BattleTank/Public/SpringWheel.h
@@ -0,0 +1,42 @@
+// Copyright Reece Adams 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "SpringWheel.generated.h"
+
+UCLASS()
+class BATTLETANK_API ASpringWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpringWheel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void SetupConstraint();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* Wheel = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UPhysicsConstraintComponent* MassWheelConstraint = nullptr;
+
+
+	
+	
+};
